a59df083b843c2d8a2fc8c2b106979f26dd46bc5
Artur Big <bigartur41@gmail.com>
2024-10-23 19:55:16 +0300

    Commit #3

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -3,14 +3,30 @@ package ru.netology.statistic;
 import org.junit.jupiter.api.Test;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 public class StatisticsServiceTest {
 
     @Test
     void findMax() {
+
         StatisticsService service = new StatisticsService();
 
         long[] incomes = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+
+        long actual = service.findMax(incomes);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMax2() {
+
+        StatisticsService service = new StatisticsService();
+
+        long[] incomes = {5, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
 
         long actual = service.findMax(incomes);
